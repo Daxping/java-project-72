dbac0361baa20f9a3c396dfe07e87b690c5ea279
Daxping <zyaalf@yandex.ru>
2023-03-01 15:06:22 +0300

    deploy

--- app/src/test/java/hexlet/code/MainTest.java
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -1,11 +1,6 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public final class MainTest {
     @Test
